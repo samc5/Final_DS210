655823753850410e26500fb8fbdef585b8d20143
samc5 <samcowan1968@gmail.com>
2024-04-21 17:51:35 -0400

    Shell of djikstras algorithm

--- proj/src/main.rs
+++ proj/src/main.rs
@@ -5,6 +5,8 @@ use std::time::{Duration, Instant};
 use std::collections::HashMap;
 use rand::Rng;
 use std::fs::OpenOptions;
+use std::collections::BinaryHeap;
+
 #[derive(Debug, Deserialize)]
 pub struct Record {
     // for reading in edges with serde
@@ -13,61 +15,77 @@ pub struct Record {
     scaled_sci:u32
 }
 
+#[derive(Debug,Clone)]
+struct Outedge {
+    vertex: String,
+    length: u32,
+}
 
-fn read_to_map(path: &str, cutoff : u32) ->  HashMap<String, Vec<(String, u32)>>{
+
+
+fn read_to_map(path: &str, cutoff : u32) ->  HashMap<String, Vec<Outedge>>{
     let rdr = csv::ReaderBuilder::new()
     .delimiter(b'\t')
     .has_headers(true)
     .flexible(true)
     .from_path(path);
-    let mut graph_list : HashMap<String, Vec<(String, u32)>> = HashMap::new();
+    let mut graph_list : HashMap<String, Vec<Outedge>> = HashMap::new();
     for result in rdr.expect("Something failed").deserialize(){ //skips first line since that's the number of vertices
         let record: Record = result.expect("Something failed");
         println!("{:?} This is a location", record.user_loc);
         //graph_list.insert(record.user_loc, (record.fr_loc, record.scaled_sci));
         if record.scaled_sci > cutoff{
-            graph_list.entry(record.user_loc).or_insert(Vec::new()).push((record.fr_loc, record.scaled_sci));
+            graph_list.entry(record.user_loc).or_insert(Vec::new()).push(Outedge{vertex: record.fr_loc, length: record.scaled_sci});
         }
             //graph_list[&record.user_loc].push((record.fr_loc, record.scaled_sci));
     }
     return graph_list
 }
 
-// fn write_test(){
-//     // DO NOT USE
-//     // Generates a file for a test, 
-//     let path = String::from("test_new.tsv");
-//     let csv_path = String::from("data/data.tsv");
-//     let mut file = File::create(&path).expect("Unable to create file");
-//     let mut file = OpenOptions::new()
-//         .append(true)
-//         .open(&path)
-//         .expect("cannot open file");
-//     let rdr = csv::ReaderBuilder::new()
-//         .delimiter(b'\t')
-//         .has_headers(true)
-//         .flexible(true)
-//         .from_path(csv_path);
-//         //let mut graph_list : HashMap<String, (String, u32)> = HashMap::new();
-//     println!("tester");
-//     for result in rdr.expect("Something failed").deserialize(){ //skips first line since that's the number of vertices
-//         let record: Record = result.expect("Something failed");
-//         println!("{:?} This is a location", record.user_loc);
-//         let rng = rand::thread_rng().gen_range(0..1000);
-//         if rng == 0{
-//             let s: String = format!("{0}\t{1}\t{2}\n", record.user_loc, record.fr_loc, record.scaled_sci);
-//             file.write_all(s.as_bytes()).expect("Unable to write file");        
-//         }
-//     }
-//     println!("Done!");
-// }
+fn shortest_paths(map: HashMap<String, Vec<Outedge>>){
+    let start: String = String::from("USA31085");
+    let mut distances: HashMap<String, Option<u32>> = HashMap::new();
+    distances.entry(start.clone()).or_insert(Some(0));
+    let mut pq = BinaryHeap::<(u32,&String)>::new();
+    pq.push((0,&start));
+
+}
+
+fn write_test(){
+    // DO NOT USE
+    // Generates a file for a test, 
+    let path = String::from("test_new.tsv");
+    let csv_path = String::from("data/data.tsv");
+    let mut file = File::create(&path).expect("Unable to create file");
+    let mut file = OpenOptions::new()
+        .append(true)
+        .open(&path)
+        .expect("cannot open file");
+    let rdr = csv::ReaderBuilder::new()
+        .delimiter(b'\t')
+        .has_headers(true)
+        .flexible(true)
+        .from_path(csv_path);
+        //let mut graph_list : HashMap<String, (String, u32)> = HashMap::new();
+    println!("tester");
+    for result in rdr.expect("Something failed").deserialize(){ //skips first line since that's the number of vertices
+        let record: Record = result.expect("Something failed");
+        println!("{:?} This is a location", record.user_loc);
+        let rng = rand::thread_rng().gen_range(0..1000);
+        if rng == 0{
+            let s: String = format!("{0}\t{1}\t{2}\n", record.user_loc, record.fr_loc, record.scaled_sci);
+            file.write_all(s.as_bytes()).expect("Unable to write file");        
+        }
+    }
+    println!("Done!");
+}
 
 
 
 fn main() {
     let start = Instant::now();
     println!("Hello, world!");
-    let adjacency_map : HashMap<String, Vec<(String, u32)>> = read_to_map("test_new.tsv", 10000000 as u32);
+    let adjacency_map : HashMap<String, Vec<Outedge>> = read_to_map("test_new.tsv", 80000 as u32);
     println!("{:?}", adjacency_map);
     let duration = start.elapsed();
     println!("Time elapsed is: {:?}", duration);
