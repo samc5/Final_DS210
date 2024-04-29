d40bd128854f2e1bbc6fc83594811862e094e405
samc5 <samcowan1968@gmail.com>
2024-04-29 13:03:40 -0400

    refactoring, MST works, fixed most if not all issues with MST not accounting for every node

--- proj/src/main.rs
+++ proj/src/main.rs
@@ -6,6 +6,7 @@ use std::collections::HashMap;
 use rand::Rng;
 use std::fs::OpenOptions;
 use std::collections::BinaryHeap;
+use std::collections::HashSet;
 
 #[derive(Debug, Deserialize)]
 pub struct Record {
@@ -40,47 +41,77 @@ type Distance = usize;
 
 #[derive(Debug)]
 struct Graph {
-  //  n: usize,
+    n: usize,
     outedges: Vec<Edge>,
-    parent: HashMap<Vertex, Vertex>,
-    rank: HashMap<Vertex, usize>,
+    parent: Vec<usize>,
+    rank: Vec<usize>,
 }
 
 impl Graph {
-    fn create_undirected(outedges:Vec<Edge>) -> Graph {
-        let parent: HashMap<Vertex, Vertex> = HashMap::new();
-        let rank: HashMap<Vertex, usize> = HashMap::new();
-        let mut g = Graph{outedges,parent,rank};
-        g.outedges.sort_by(|a, b| b.2.cmp(&a.2)); // does this sort by minimum?
-        for node in &g.outedges {
-            println!("{:?} hey", node.0);
-            let new_node0 = &node.0;
-            let new_node1 = &node.1;
-            g.parent.insert(String::from(new_node1), String::from(new_node0));
-            g.rank.insert(String::from(new_node0), 0);
+    fn create_undirected(n:usize,outedges:Vec<Edge>) -> Graph {
+        let parent: Vec<usize> = vec![];
+        let rank: Vec<usize> = vec![];
+        let mut g = Graph{n,outedges,parent,rank};
+        g.outedges.sort_by(|a, b| b.2.cmp(&a.2));
+        for node in 0..g.n {
+            g.parent.push(node);
+            g.rank.push(0);
         }
-        return g
+        g
     }
-    
-    // fn find(&mut self, i:Vertex) -> Vertex {
-    //     if self.parent[i] != i {
-    //         self.parent[i] = self.find(self.parent[i]);
-    //     }
-    //     return self.parent[i];
-    // }
+    fn find(&mut self, i:usize) -> usize {
+        if self.parent[i] != i {
+            self.parent[i] = self.find(self.parent[i]);
+        }
+        return self.parent[i];
+    }
+    fn union(&mut self, i:usize, j:usize) {
+        if self.rank[i] < self.rank[j] {
+            self.parent[i] = j;
+        } else if self.rank[i] > self.rank[j] {
+            self.parent[j] = i;
+        } else {
+            self.parent[j] = i;
+            self.rank[i] += 1;
+        }
+    }
+    fn KruskalMST(&mut self, vec_to_num_map: HashMap<String, usize>) -> Vec<Edge> {
+        let mut result: Vec<Edge> = vec![];
+        let mut num_mst_e = 0;
+        let mut next_edge = 0;
+        while num_mst_e < self.n - 1 {
+            let (u,v,w) = &self.outedges[next_edge];
+           // println!("{:?}", u);
+           // println!("{:?}", u);
+            match vec_to_num_map.get(v){
+                None => {
+                    //num_mst_e += 1;
+                    next_edge = next_edge + 1;
+                    println!("MONENONENONENONE AT {:?}", v);
+                },
+                Some(v_numeric) => {
+                    println!("{:?} out of {:?}", num_mst_e, self.n);
+                    let u_numeric = vec_to_num_map.get(u).unwrap();
+                    let new_w : usize = *w;
+                    let new_u : String = String::from(u);
+                    let new_v : String = String::from(v);
+                    next_edge = next_edge + 1;
+                    
+                    let x = self.find(*u_numeric);
+                    let y = self.find(*v_numeric);
+                    if x != y {
+                        num_mst_e += 1;
+                        result.push((new_u, new_v, new_w));
+                        self.union(x,y);
+                    }
+                }
+            }
+            //let u_numeric = vec_to_num_map.get(u).unwrap();
 
-    // fn union(&mut self, i:Vertex, j:Vertex) {
-    //     if self.rank[i] < self.rank[j] {
-    //         self.parent[i] = j;
-    //     } else if self.rank[i] > self.rank[j] {
-    //         self.parent[j] = i;
-    //     } else {
-    //         self.parent[j] = i;
-    //         self.rank[i] += 1;
-    //     }
-    // }
+        }
+        result
+    }
 }
-
 
 
 
@@ -99,34 +130,6 @@ fn counties_map() -> HashMap<String, String>{
     }
     return county_map;
 }
-
-
-
-fn read_to_map(path: &str, cutoff : usize) ->  HashMap<String, Vec<Outedge>>{
-    let rdr = csv::ReaderBuilder::new()
-    .delimiter(b'\t')
-    .has_headers(true)
-    .flexible(true)
-    .from_path(path);
-    let mut graph_list : HashMap<String, Vec<Outedge>> = HashMap::new();
-    let county_map = counties_map();
-    for result in rdr.expect("Something failed").deserialize(){ 
-        let record: Record = result.expect("Something failed");
-        let level = match county_map.get(&record.user_loc){
-            None => "None",
-            Some(val) => {val}
-        };
-      //  println!("{:?}", level);
-        //println!("{:?} This is a location", record.user_loc);
-        if record.scaled_sci > cutoff{
-            graph_list.entry(record.user_loc).or_insert(Vec::new()).push(Outedge{vertex: record.fr_loc, length: record.scaled_sci});
-        }
-            //graph_list[&record.user_loc].push((record.fr_loc, record.scaled_sci));
-    }
-    return graph_list
-}
-
-
 
 
 fn read_to_counts(path: &str, county_map: HashMap<String, String>) -> HashMap<(String, String), CountryPair>{
@@ -177,6 +180,16 @@ fn read_to_counts(path: &str, county_map: HashMap<String, String>) -> HashMap<(S
     
 }
 
+// fn read_to_counts_updated(path: &str, county_map: HashMap<String, String>) -> HashMap<(String, String), CountryPair>{
+//     let rdr = csv::ReaderBuilder::new()
+//     .delimiter(b'\t')
+//     .has_headers(true)
+//     .flexible(true)
+//     .from_path(path);
+//     let mut counts_map : HashMap<(String, String), CountryPair> = HashMap::new();
+// }
+
+
 
 fn counts_to_hashmap(counts_map: HashMap<(String, String), CountryPair>) -> HashMap<String, Vec<Outedge>>{
     let mut graph_list : HashMap<String, Vec<Outedge>> = HashMap::new();
@@ -190,16 +203,26 @@ fn counts_to_hashmap(counts_map: HashMap<(String, String), CountryPair>) -> Hash
     return graph_list
 }
 
-fn counts_to_vector(counts_map: HashMap<(String, String), CountryPair>) -> Vec<Edge>{
+fn counts_to_vector(counts_map: HashMap<(String, String), CountryPair>) -> (Vec<Edge>, HashMap<String, usize>, HashMap<usize, String>, usize){
+    let mut vec_to_num_map : HashMap<String, usize> = HashMap::new();
+    let mut num_to_vec_map : HashMap<usize, String> = HashMap::new();
     let mut graph_list : Vec<Edge> = Vec::new();
+    let mut counter : usize = 0;
     for (key, val) in counts_map.iter(){
         let pair : &CountryPair = val;
         let true_distance : usize = (*pair).distance / (*pair).count; // I know this is integer division but it shouldn't lose much precision and I feel like converting to floats and back would add a fair amount of operations
         let vertex1 = &key.0;
         let vertex2 = &key.1;
+        if let None = vec_to_num_map.get(vertex1){
+            vec_to_num_map.insert(String::from(vertex1), counter);
+            num_to_vec_map.insert(counter, String::from(vertex1));
+            counter += 1;
+
+        }
         graph_list.push((String::from(vertex1), String::from(vertex2), true_distance));
+        
     }   
-    return graph_list
+    return (graph_list, vec_to_num_map, num_to_vec_map, counter);
 }
 
 
@@ -208,69 +231,11 @@ fn read_to_map_aggregate(path: &str) ->  HashMap<String, Vec<Outedge>>{
     return counts_to_hashmap(counts_map);
 }
 
-fn read_to_vec_aggregate(path: &str) ->  Vec<Edge>{
+fn read_to_vec_aggregate(path: &str) ->  (Vec<Edge>, HashMap<String, usize>, HashMap<usize, String>, usize){
     let counts_map : HashMap<(String, String), CountryPair> = read_to_counts(path, counties_map());
-    return counts_to_vector(counts_map);
-}
-
-
-
-
-
-
-fn shortest_paths(map: &HashMap<String, Vec<Outedge>>, start : String){
-    // Repurposed from class to work with a hashmap that includes an Outedge (a weighted edge)
-    let mut distances: HashMap<&String, Option<usize>> = HashMap::new();
-    distances.entry(&start).or_insert(Some(0));
-    let mut pq = BinaryHeap::<(usize,&String)>::new();
-    pq.push((0,&start));
-    // the real stf
-    while let Some((dist,v)) = pq.pop() {
-        let rng = rand::thread_rng().gen_range(0..500);
-        // new problem seems to do with cycles?
-        // if rng == 0{
-        //     println!("!---!");
-        //     println!("The while loop is at the top, the some is {:?}, {:?}", dist, v);
-        // //    println!("{:?}", pq);
-        // }
-        match map.get(v) {
-            None => {break},
-            Some(edges) => {
-                for Outedge{vertex,length} in edges.iter() {
-                    // if rng == 0{
-                    //     println!("{:?}, {:?}", vertex, v);
-                    // }
-                    let new_dist = dist + *length;
-                    if rng == 0{
-                       // println!("{:?}", distances.get(vertex));
-                    }
-                    let update = match distances.get(vertex) {
-                        None => {true}
-                        Some(d) => {
-                            match d{
-                                None => {false}, //changed this from true?
-                                Some(real_d) => v != vertex && {new_dist > *real_d} // the first part of the and is to account for the vertex being connected to itself
-                            }
-                        }
-                    };
-                    if update {
-                        distances.entry(vertex).or_insert(Some(new_dist)); //may be missing something
-                        pq.push((new_dist,vertex));
-                    }
-                }
-            }
-        };
-        if rng == 0 {
-          println!("!---!");
-          //  println!("The while loop is at the top, the some is {:?}, {:?}", dist, v);
-           println!("{:?}", pq.len());
-           break;
-           // println!("{:?}", distances);
-        }
-
-    };
-//    println!("{:?}", distances);
-
+    let tup :  (Vec<Edge>, HashMap<String, usize>, HashMap<usize, String>, usize) = counts_to_vector(counts_map);
+    println!("{:?}", tup.3);
+    return tup;
 }
 
 fn write_test(){
@@ -308,16 +273,23 @@ fn main() {
     let start = Instant::now();
    // println!("Hello, world!");
   //  let adjacency_map : HashMap<String, Vec<Outedge>> = read_to_map_aggregate("test_new.tsv");
-    let edge_list : Vec<Edge> = read_to_vec_aggregate("test_new.tsv");
-    let mut g : Graph = Graph::create_undirected(edge_list);
-    println!("{:?}", g);
-    // let adjacency_map : HashMap<String, Vec<Outedge>> = read_to_map_aggregate("data/data.tsv", 10000 as usize);
-
-    //let adjacency_map : HashMap<String, Vec<Outedge>> = read_to_map("data/data.tsv", 100000 as usize);
-    // for i in adjacency_map.keys(){
-    //    // println!("{:?}", i);
-    //     shortest_paths(&adjacency_map, String::from(i));
-    // }
+    let vec_tuple : (Vec<Edge>, HashMap<String, usize>, HashMap<usize, String>, usize) = read_to_vec_aggregate("data/copy-updated.tsv");
+    println!("Creating undirected");
+    let mut g : Graph = Graph::create_undirected(vec_tuple.3, vec_tuple.0);
+    println!("MST going");
+    let path = String::from("output_MST.tsv");
+    let mut file = File::create(&path).expect("Unable to create file");
+    let mut file = OpenOptions::new()
+        .append(true)
+        .open(&path)
+        .expect("cannot open file");
+    let mst : Vec<Edge> = g.KruskalMST(vec_tuple.1);
+    for i in &mst{
+        println!("{:?} - {:?} -> {:?}", i.0, i.1, i.2);
+        let s: String = format!("{0}\t{1}\t{2}\n", i.0, i.1, i.2);
+        file.write_all(s.as_bytes()).expect("Unable to write file");        
+    }
+    // println!("MST result: {:?}", mst);
     let counties_map = counties_map();
     //println!("{:?}", counties_map);
     let duration = start.elapsed();
